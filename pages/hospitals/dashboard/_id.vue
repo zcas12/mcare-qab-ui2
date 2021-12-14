bc05c161bcd8447957ef809b1ac6359b86bfbc0b
Jaehyeok PC <zcad8546@naver.com>
2021-12-14 15:27:58 +0900

    Initial commit

---
+++ pages/hospitals/dashboard/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="contents-container column pa-5">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "_id"
+}
+</script>
+
+<style scoped>
+.contents-container {
+  min-width: 500px;
+}
+</style>
---
+++ pages/hospitals/dashboard/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="contents-container column pa-5">
+    <div class="row flex-warp align-center">
+      <h2>병원 QAB Dashboard</h2>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data: () => ({
+    search: null,
+    toggle: false
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
